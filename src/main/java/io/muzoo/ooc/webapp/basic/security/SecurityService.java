2f63365c0e5ae350efce94d916ba4eb20997711e
injurio <49044584+injurio@users.noreply.github.com>
2021-07-10 21:45:52 +0700

    Update SecurityService.java

--- src/main/java/io/muzoo/ooc/webapp/basic/security/SecurityService.java
+++ src/main/java/io/muzoo/ooc/webapp/basic/security/SecurityService.java
@@ -1,15 +1,22 @@
 package io.muzoo.ooc.webapp.basic.security;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Objects;
 
 public class SecurityService {
 
+    private MySql mySql;
     private UserService userService;
 
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    public UserService getUserService() {
+        return userService;
     }
 
     public String getCurrentUsername(HttpServletRequest request) {
@@ -32,14 +39,20 @@ public class SecurityService {
     public boolean login(HttpServletRequest request) {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        User user = userService.findByUsername(username);
-        if (user != null && Objects.equals(user.getPassword(), password)) {
+        User user = userService.getUser(username);
+        if (user != null && BCrypt.checkpw(password, userService.getUser(username).getPassword())) {
             HttpSession session = request.getSession();
             session.setAttribute("username", username);
             return true;
         } else {
             return false;
         }
-
+//        if (user != null && Objects.equals(user.getPassword(), password)) {
+//            HttpSession session = request.getSession();
+//            session.setAttribute("username", username);
+//            return true;
+//        } else {
+//            return false;
+//        }
     }
 }
