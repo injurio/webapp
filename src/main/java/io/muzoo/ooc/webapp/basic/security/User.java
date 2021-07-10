0d07185698656784ff66c214ac1760189a898315
injurio <49044584+injurio@users.noreply.github.com>
2021-07-10 21:46:24 +0700

    Update User.java

--- src/main/java/io/muzoo/ooc/webapp/basic/security/User.java
+++ src/main/java/io/muzoo/ooc/webapp/basic/security/User.java
@@ -2,16 +2,37 @@ package io.muzoo.ooc.webapp.basic.security;
 
 public class User {
 
+    private Long id;
+
     private String username;
 
     private String password;
 
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+
+
     public User() {
     }
 
-    public User(String username, String password) {
+    public User(String username, String password, String email, String firstName, String lastName) {
         this.username = username;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getUsername() {
@@ -28,5 +49,29 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
