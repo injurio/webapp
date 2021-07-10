05d50f86bd24f0484ae1cdf5759be0b8f3492b62
injurio <49044584+injurio@users.noreply.github.com>
2021-07-10 21:37:42 +0700

    Update HomeServlet.java

--- src/main/java/io/muzoo/ooc/webapp/basic/servlets/HomeServlet.java
+++ src/main/java/io/muzoo/ooc/webapp/basic/servlets/HomeServlet.java
@@ -1,10 +1,10 @@
 package io.muzoo.ooc.webapp.basic.servlets;
 
-import javax.servlet.RequestDispatcher;
+import io.muzoo.ooc.webapp.basic.AbstractRoutableHttpServlet;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class HomeServlet extends AbstractRoutableHttpServlet {
@@ -12,15 +12,10 @@ public class HomeServlet extends AbstractRoutableHttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if (securityService.isAuthorized(request)) {
-            String username = securityService.getCurrentUsername(request);
-            request.setAttribute("username", username);
-
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/home.jsp");
-            requestDispatcher.include(request, response);
+            response.sendRedirect("/user");
         } else {
             response.sendRedirect("/login");
         }
-
     }
 
     @Override
