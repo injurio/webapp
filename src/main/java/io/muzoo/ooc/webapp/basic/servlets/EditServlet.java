9191dd7f4cca037a44c60468391ae7a78d03cad3
injurio <49044584+injurio@users.noreply.github.com>
2021-07-10 21:37:01 +0700

    Create EditServlet.java

---
+++ src/main/java/io/muzoo/ooc/webapp/basic/servlets/EditServlet.java
@@ -0,0 +1,43 @@
+  
+package io.muzoo.ooc.webapp.basic.servlets;
+
+import io.muzoo.ooc.webapp.basic.AbstractRoutableHttpServlet;
+import io.muzoo.ooc.webapp.basic.security.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EditServlet extends AbstractRoutableHttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (securityService.isAuthorized(request)) {
+            String username = request.getParameter("username");
+            User currentUser = userService.getUser(username);
+            request.setAttribute("currentUser", currentUser);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/edit.jsp");
+            requestDispatcher.include(request, response);
+        } else {
+            response.sendRedirect("/");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        userService.editUser(username, password, email, firstName, lastName);
+        response.sendRedirect("/user");
+    }
+
+    @Override
+    public String getPattern() {
+        return "/edit";
+    }
+}
